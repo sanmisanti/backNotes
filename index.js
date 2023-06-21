6d5451e852bafa5d100d8230b2dbdede663c575a
sanmisanti <sanmisanti@gmail.com>
2023-06-21 14:31:15 -0300

    singleQuote

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ let notes = [
 ];
 
 /* const requestListener = function (req, res) {
-  res.writeHead(200, { "Content-Type": "application/json" });
+  res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify(notes));
 }; */
 
