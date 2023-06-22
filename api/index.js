cab07cca63c2d314630e838f9aac444be429f74f
sanmisanti <sanmisanti@gmail.com>
2023-06-22 20:41:44 -0300

    me falto un require

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const http = require("http");
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
+
 app.use(express.static(path.join(__dirname, "../dist")));
 
 app.use(express.json());
