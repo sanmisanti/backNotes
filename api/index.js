8003326e217ff9e2cce67cbd2df0a46d6c985ef1
sanmisanti <sanmisanti@gmail.com>
2023-06-22 15:18:34 -0300

    chagne1

--- api/index.js
+++ api/index.js
@@ -3,6 +3,11 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+app.use(express.json());
+
+app.use(cors());
+app.use(express.static("dist"));
+
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
   console.log("Path:  ", request.path);
@@ -11,10 +16,7 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-app.use(express.json());
 app.use(requestLogger);
-app.use(cors());
-app.use(express.static("dist"));
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
