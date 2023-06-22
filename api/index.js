f29b13ca1e27415f5b17f5afe941321828940123
sanmisanti <sanmisanti@gmail.com>
2023-06-22 17:33:21 -0300

    le agregue un punto si funciona me m

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 app.use(express.json());
 
 app.use(cors());
-app.use(express.static("../dist"));
+app.use(express.static("./../dist"));
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
